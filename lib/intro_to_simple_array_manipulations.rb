9ae00f87a3739d66d863742d039fea2d28d473b3
Youngmin Park <codepark2019@gmail.com>
2020-04-10 02:53:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
-def using_concat(my_favorite_thing, more_favs)
-  my_favorite_thing.concat(more_favs)
+def using_concat(my_favorite_thin, more_favs)
+  my_favorite_thin.concat(more_favs)
 end
 
 def using_insert(list_of_programming_languages, another_language)
